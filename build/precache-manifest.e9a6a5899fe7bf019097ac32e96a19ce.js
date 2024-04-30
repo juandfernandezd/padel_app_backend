e930ad162dede1b84520a879bbb8646d4d23218e
Juan David Fernández Díaz <juandfernandez@outlook.com>
2024-04-29 23:08:38 -0500

    add build folder

---
+++ build/precache-manifest.e9a6a5899fe7bf019097ac32e96a19ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74c216d09a5cfc0e3bc9dbb669beb28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c25b89baeef52730db",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "648c1499e8352dcede72",
+    "url": "/static/css/main.809d43a1.chunk.css"
+  },
+  {
+    "revision": "08c25b89baeef52730db",
+    "url": "/static/js/2.3a79f52c.chunk.js"
+  },
+  {
+    "revision": "a70cfba45799f8a4deb5",
+    "url": "/static/js/3.cf2aeaea.chunk.js"
+  },
+  {
+    "revision": "648c1499e8352dcede72",
+    "url": "/static/js/main.1db8625f.chunk.js"
+  },
+  {
+    "revision": "9345484f00c714b2184c",
+    "url": "/static/js/runtime~main.94c5bc57.js"
+  },
+  {
+    "revision": "df8665d3e226b24d8a296cb34a3692fd",
+    "url": "/static/media/DigitalNumbers-Regular.df8665d3.ttf"
+  },
+  {
+    "revision": "764a60419efe6d853b1ec11cfee0ac5d",
+    "url": "/static/media/Rufina-Regular.764a6041.ttf"
+  }
+]);
