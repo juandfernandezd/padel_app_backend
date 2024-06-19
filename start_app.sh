b628ea2409b4c471ed1fa56c74699d7b82510260
Juan David Fernández Díaz <juandfernandez@outlook.com>
2024-06-18 22:03:38 -0500

    add venv activation to start script

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export DISPLAY=:0
 # Iniciar el daemon de pigpio
 echo "Iniciando pigpio daemon..."
 sudo pigpiod
@@ -7,7 +7,7 @@ sudo pigpiod
 # Ejecutar el servidor FastAPI
 echo "Iniciando el servidor FastAPI..."
 cd "$(dirname "$0")"
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+python3 ./main.py &
 
 # Esperar un momento para asegurarse de que el servidor FastAPI ha iniciado
 sleep 5
@@ -24,6 +24,12 @@ npm start &
 # Esperar un momento para asegurarse de que la aplicación React ha iniciado
 sleep 10
 
-# Abrir Firefox en modo pantalla completa apuntando a la aplicación React
-echo "Abriendo Firefox en modo pantalla completa..."
-firefox --kiosk http://localhost:3000
+echo "Cerrando instancias existentes de Firefox..."
+pkill -f firefox
+
+# Crear un nuevo perfil temporal para Firefox
+PROFILE_DIR=$(mktemp -d)
+firefox --no-remote --profile "$PROFILE_DIR" --kiosk http://localhost:3000 &
+
+# Limpiar el perfil temporal al salir
+trap 'rm -rf "$PROFILE_DIR"' EXIT
